568909ca93fa7266f934e04f230fd765deaca7e3
m-faisal930 <faisal.mr333@gmail.com>
2024-12-07 11:16:15 +0500

    adding modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -21,7 +21,7 @@ app.use(cors({
 const port = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes);
-app.use('/api/message', messageRoutes);
+
 
 
 app.listen(port, () => {
