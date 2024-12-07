ce87ff6f71ea62b3f9d2035328e030a11652faa7
m-faisal930 <faisal.mr333@gmail.com>
2024-12-07 11:24:44 +0500

    adding features folder

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout, updateProfile, checkAuth } from '../controllers/auth.controlller.js';
+import { signup, login, logout, checkAuth } from '../controllers/auth.controlller.js';
 import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -7,7 +7,6 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login',login);
 router.post('/logout', logout);
-router.put('/update-profile', protectRoute , updateProfile);
 router.get('/check', protectRoute, checkAuth);
 
 export default router;
